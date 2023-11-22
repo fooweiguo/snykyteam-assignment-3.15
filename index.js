8ac7a4073d19e426e8e3325ffa500d28eebad399
Emmanuel Rebellon <emman.rebellon@gmail.com>
2023-11-22 20:18:49 +0800

    Person 3 (Emmanuel) add index.js

--- index.js
+++ index.js
@@ -1,33 +1,13 @@
-// before rum
-//npm install express
-​
-// to run 
-// npm start
-​
-'use strict';
-​
-const express = require('express');
-​
-// Constants
-const PORT = 8080;
-const HOST = '127.0.0.1';
-const OS = require('os');
-const ENV = 'DEV';
-​
-​
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello from Martin! Running Node.js!';
-  res.send(msg);
-});
-​
-app.get('/test', (req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello from /test Node!';
-  res.send(msg);
-});
-​
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+module.exports.handler = async (event) => {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: "Go Serverless v3.0! Your function executed successfully!",
+          input: event,
+        },
+        null,
+        2
+      ),
+    };
+  };
